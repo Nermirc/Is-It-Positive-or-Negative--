27c694d5241300b847c9ffc1596795ad5c1ebe35
Nermirc <141329241+Nermirc@users.noreply.github.com>
2023-10-29 17:44:24 +0300

    Is It Positive or Negative ?
    finding positive or negative number  by using if else conditions

---
+++ main.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int a,b;
+    printf("Lutfen iki adet sayi giriniz.\n");
+    scanf("%d %d",&a,&b);
+
+    if((a== 0 || (b==0))){
+       printf("Carpim degeri 0'dir. \n");
+    }
+    else if((a>0 && b>0) || (a<0 && b<0){
+            printf("Carpim degeri pozitiftir.\n");
+
+    }
+    else{
+        printf("Carpim degeri negatiftir.");
+    }
+}
